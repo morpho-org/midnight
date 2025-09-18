33ca2ed8b7accadbe17cb2efade20228c40766c6
peyha <peyha.nicole@outlook.fr>
2025-09-18 15:41:19 +0200

    test: late auction fix

--- test/PostMaturityLiquidationTest.sol
+++ test/PostMaturityLiquidationTest.sol
@@ -105,15 +105,16 @@ contract PostMaturityLiquidationTest is BaseTest {
         setupBond(term, 100, 200);
         deal(address(loanToken), address(this), 1);
 
-        vm.warp(term.maturity + 3600);
+        vm.warp(term.maturity + 3800);
 
         // Test
         Seizure[] memory seizures = new Seizure[](2);
         seizures[0] = Seizure({repaidBonds: 1, seizedAssets: 0});
         seizures[1] = Seizure({repaidBonds: 0, seizedAssets: 0});
-        terms.postMaturityLiquidation(term, seizures, borrower, "");
-        assertEq(terms.debtOf(borrower, id), 99);
-        assertEq(terms.collateralOf(borrower, id, term.collaterals[0].token), 196);
+        Seizure[] memory res = terms.postMaturityLiquidation(term, seizures, borrower, "");
+        console.log(res[0].repaidBonds, res[0].seizedAssets);
+        assertEq(terms.debtOf(borrower, id), 39);
+        assertEq(terms.collateralOf(borrower, id, term.collaterals[0].token), 195);
         assertEq(loanToken.balanceOf(address(this)), 0);
     }
 }
