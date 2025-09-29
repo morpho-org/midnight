eb9c84df288025bc49f76fb00109099acca005a2
peyha <peyha.nicole@outlook.fr>
2025-09-29 18:34:41 +0200

    feat: improve log precision

--- src/libraries/LogLib.sol
+++ src/libraries/LogLib.sol
@@ -10,8 +10,16 @@ library LogLib {
 
     /// @dev Returns an approximation of ln.
     function wLn(int256 x) internal pure returns (int256 y) {
-        unchecked {
-            y = x - x * x / (2 * WAD_INT) + x * x / WAD_INT * x / (3 * WAD_INT);
+        uint256 N_ROUNDS = 10;
+        assembly {
+            let acc := x
+
+            for { let i := 0 } lt(i, N_ROUNDS) { i := add(i, 1) } {
+                switch mod(i, 2)
+                case 0 { y := add(y, div(acc, add(i, 1))) }
+                default { y := sub(y, div(acc, add(i, 1))) }
+                acc := div(mul(x, acc), WAD_INT)
+            }
         }
     }
 }
