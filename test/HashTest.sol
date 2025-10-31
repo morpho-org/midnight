9462b8666a6f8438b3c6b6c8167592210543865b
Quentin Garchery <garchery.quentin@gmail.com>
2025-10-31 19:15:31 +0100

    chore: remove useless test data

--- test/HashTest.sol
+++ test/HashTest.sol
@@ -17,25 +17,6 @@ contract HashTest is BaseTest {
         collaterals[0] = Collateral({token: address(0x8), lltv: 10, oracle: address(0x9)});
         obligation.collaterals = collaterals;
 
-        bytes memory encoded = abi.encode(obligation);
-        for(uint256 i = 32; i <= encoded.length; i+=32) {
-            bytes32 value;
-            assembly {
-                value := mload(add(encoded, i))
-            }
-            console.logBytes32(value);
-        }
-        console.log("--------------------------------");
-
         assertEq(UtilsLib.hashObligation(obligation), keccak256(abi.encode(obligation)));
-        // assertEq(UtilsLib.hashObligation2(obligation), keccak256(abi.encode(obligation)));
-        uint256 length = 32 * 6 + 3 * 32 * obligation.collaterals.length;
-        for(uint256 i; i < length; i+=32) {
-            bytes32 value;
-            assembly {
-                value := mload(add(obligation, i))
-            }
-            console.logBytes32(value);
-        }
     }
 }
