7fcf074348a048be5d77660a8dc5d57c6c26e9ec
MathisGD <mathis@morpho.xyz>
2026-02-03 15:22:15 +0100

    chore: solc version

--- src/MorphoV2.sol
+++ src/MorphoV2.sol
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: BUSL-1.1
 // Copyright (c) 2025 Morpho Association
-pragma solidity 0.8.28;
+pragma solidity 0.8.31;
 
 import {UtilsLib} from "./libraries/UtilsLib.sol";
 import {TickLib} from "./libraries/TickLib.sol";
