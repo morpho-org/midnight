dd78e4a5cf81aa774751c4a4c5a7804e60d79a7d
peyha <peyha.nicole@outlook.fr>
2026-03-17 18:16:52 +0100

    doc: unit

--- src/interfaces/IMidnight.sol
+++ src/interfaces/IMidnight.sol
@@ -10,7 +10,7 @@ struct Obligation {
     // The recovery close factor is deactivated for a collateral if the liquidation could leave a collateral value that
     // would not be enough to repay rcfThreshold units.
     // rcfThreshold is scaled by BALANCE_DECIMALS.
-    uint256 rcfThreshold; // 1 rcfThreshold unit = 1e-6 loan token units
+    uint256 rcfThreshold; // 1 rcfThreshold unit = 1e-6 loan token
 }
 
 struct Collateral {
@@ -24,7 +24,7 @@ struct Offer {
     Obligation obligation;
     bool buy;
     address maker;
-    uint256 obligationUnits; // 1 obligation unit = 1e-6 loan token units
+    uint256 obligationUnits; // 1 obligation unit = 1e-6 loan token
     uint256 start;
     uint256 expiry;
     uint256 tick;
