87d8e9925d0820a92676556ead9d28b34bcb9a63
peyha <peyha.nicole@outlook.fr>
2026-04-08 17:17:39 +0200

    test: pragma

--- test/ContinuousFeeTest.sol
+++ test/ContinuousFeeTest.sol
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: GPL-2.0-or-later
 // Copyright (c) 2025 Morpho Association
-pragma solidity ^0.8.0;
+pragma solidity ^0.8.4;
 
 import {WAD, MAX_CONTINUOUS_FEE} from "../src/libraries/ConstantsLib.sol";
 import {EventsLib} from "../src/libraries/EventsLib.sol";
