acf02b0fc7de57036e41826112165447335f9a0e
peyha <peyha.nicole@outlook.fr>
2026-04-15 17:18:50 +0200

    refactor: custom error

--- src/ratifiers/interfaces/ISetterRatifier.sol
+++ src/ratifiers/interfaces/ISetterRatifier.sol
@@ -6,6 +6,7 @@ import {IRatifier} from "../../interfaces/IRatifier.sol";
 
 interface ISetterRatifier is IRatifier {
     /// ERRORS ///
+    error Unauthorized();
     error NotRatified();
 
     /// EVENTS ///
