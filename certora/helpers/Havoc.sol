f1412b1cbf2306c5a3f1d81836087aa3fdec1cb2
peyha <peyha.nicole@outlook.fr>
2026-04-15 17:16:04 +0200

    chore: pragma

--- certora/helpers/Havoc.sol
+++ certora/helpers/Havoc.sol
@@ -1,13 +1,12 @@
 // SPDX-License-Identifier: GPL-2.0-or-later
 // Copyright (c) 2025 Morpho Association
-pragma solidity ^0.8.4;
+pragma solidity ^0.8.0;
 
 interface IHavoc {
     function havocAll() external;
 }
 
 contract Havoc {
-
     function callHavoc(address account) external {
         (IHavoc(account)).havocAll();
     }
