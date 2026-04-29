4879acff7a9dbad2bfe4ba17e5ac7be8225bb656
MathisGD <mathis@morpho.xyz>
2026-04-29 15:57:43 +0200

    takeAmountLib reverts if offer is untakeable

--- src/periphery/TakeAmountsLib.sol
+++ src/periphery/TakeAmountsLib.sol
@@ -20,7 +20,8 @@ library TakeAmountsLib {
         uint256 offerPrice = TickLib.tickToPrice(offer.tick);
         uint256 tradingFee =
             IMidnight(midnight).tradingFee(id, UtilsLib.zeroFloorSub(offer.obligation.maturity, block.timestamp));
-        uint256 buyerPrice = offer.buy ? offerPrice : offerPrice + tradingFee;
+        uint256 sellerPrice = offer.buy ? offerPrice - tradingFee : offerPrice;
+        uint256 buyerPrice = sellerPrice + tradingFee;
         require(buyerPrice <= WAD, TickLib.PriceGreaterThanOne());
         return offer.buy ? targetBuyerAssets.mulDivUp(WAD, buyerPrice) : targetBuyerAssets.mulDivDown(WAD, buyerPrice);
     }
