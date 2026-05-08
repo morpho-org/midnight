7b34d860aa84a04788aa3f5279198e126de4e033
MathisGD <74971347+MathisGD@users.noreply.github.com>
2026-05-08 16:34:44 +0200

    Update src/interfaces/IMidnight.sol
    Co-authored-by: Adrien Husson <adhusson@gmail.com>
    Signed-off-by: MathisGD <74971347+MathisGD@users.noreply.github.com>

--- src/interfaces/IMidnight.sol
+++ src/interfaces/IMidnight.sol
@@ -37,7 +37,7 @@ struct Offer {
     uint256 maxBuyerAssets;
 }
 
-/// @dev Trading fees are 0 until the obligation is created, then set to the default values.
+/// @dev Trading fees and the continuous fee are 0 until the obligation is created, then set to the default values.
 struct ObligationState {
     uint128 totalUnits;
     uint128 lossFactor;
