a6191d236c9fb086d9a481d48919b59cf0f29d61
Quentin Garchery <garchery.quentin@gmail.com>
2026-05-12 17:38:44 +0200

    fix lint error

--- certora/helpers/Utils.sol
+++ certora/helpers/Utils.sol
@@ -5,7 +5,7 @@ pragma solidity ^0.8.0;
 import {Offer} from "../../src/interfaces/IMidnight.sol";
 import {Obligation} from "../../src/interfaces/IMidnight.sol";
 import {UtilsLib} from "../../src/libraries/UtilsLib.sol";
-import {CALLBACK_SUCCESS, maxTradingFee} from "../../src/libraries/ConstantsLib.sol";
+import {CALLBACK_SUCCESS, maxTradingFee as _maxTradingFee} from "../../src/libraries/ConstantsLib.sol";
 
 contract Utils {
     function hashObligation(Obligation memory obligation) external pure returns (bytes32) {
@@ -42,6 +42,6 @@ contract Utils {
     }
 
     function maxTradingFee(uint256 index) external pure returns (uint256) {
-        return maxTradingFee(index);
+        return _maxTradingFee(index);
     }
 }
