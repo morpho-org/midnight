a241a9571f25f79b656bb39b852adc6f544de507
MathisGD <74971347+MathisGD@users.noreply.github.com>
2026-05-12 17:00:38 +0200

    Apply suggestion
    Signed-off-by: MathisGD <74971347+MathisGD@users.noreply.github.com>

--- src/libraries/ConstantsLib.sol
+++ src/libraries/ConstantsLib.sol
@@ -50,14 +50,6 @@ function isLltvAllowed(uint256 lltv) pure returns (bool) {
 
 /// @dev Returns the max trading fee for the given index.
 function maxTradingFee(uint256 index) pure returns (uint256) {
-    return [
-        MAX_TRADING_FEE_0_DAYS,
-        MAX_TRADING_FEE_1_DAY,
-        MAX_TRADING_FEE_7_DAYS,
-        MAX_TRADING_FEE_30_DAYS,
-        MAX_TRADING_FEE_90_DAYS,
-        MAX_TRADING_FEE_180_DAYS,
-        MAX_TRADING_FEE_360_DAYS
-    ][index];
+    return [MAX_TRADING_FEE_0_DAYS, MAX_TRADING_FEE_1_DAY, MAX_TRADING_FEE_7_DAYS, MAX_TRADING_FEE_30_DAYS, MAX_TRADING_FEE_90_DAYS, MAX_TRADING_FEE_180_DAYS, MAX_TRADING_FEE_360_DAYS][index];
 }
 // forgefmt: disable-end
