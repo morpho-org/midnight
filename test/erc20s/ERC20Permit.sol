a1d881c373c9bee30e8ab9fc9c2246fb8442de93
MathisGD <74971347+MathisGD@users.noreply.github.com>
2026-05-12 12:03:25 +0200

    fmt

--- test/erc20s/ERC20Permit.sol
+++ test/erc20s/ERC20Permit.sol
@@ -3,34 +3,12 @@
 pragma solidity ^0.8.0;
 
 import {ERC20} from "./ERC20.sol";
+import {PermitExt} from "./PermitExt.sol";
 
-contract ERC20Permit is ERC20 {
-    bytes32 public constant PERMIT_TYPEHASH =
-        keccak256("Permit(address owner,address spender,uint256 value,uint256 nonce,uint256 deadline)");
+contract ERC20Permit is ERC20, PermitExt {
+    constructor(string memory _name, string memory _symbol) ERC20(_name, _symbol) PermitExt(_name) {}
 
-    mapping(address => uint256) public nonces;
-
-    constructor(string memory _name, string memory _symbol) ERC20(_name, _symbol) {}
-
-    function DOMAIN_SEPARATOR() public view returns (bytes32) {
-        return keccak256(
-            abi.encode(
-                keccak256("EIP712Domain(string name,string version,uint256 chainId,address verifyingContract)"),
-                keccak256(bytes(name)),
-                keccak256("1"),
-                block.chainid,
-                address(this)
-            )
-        );
-    }
-
-    function permit(address owner, address spender, uint256 value, uint256 deadline, uint8 v, bytes32 r, bytes32 s)
-        external
-    {
-        require(block.timestamp <= deadline, "permit expired");
-        bytes32 structHash = keccak256(abi.encode(PERMIT_TYPEHASH, owner, spender, value, nonces[owner]++, deadline));
-        bytes32 digest = keccak256(abi.encodePacked("\x19\x01", DOMAIN_SEPARATOR(), structHash));
-        require(ecrecover(digest, v, r, s) == owner, "invalid permit");
+    function _setAllowance(address owner, address spender, uint256 value) internal override {
         allowance[owner][spender] = value;
     }
 }
--- test/erc20s/PermitExt.sol
+++ test/erc20s/PermitExt.sol
@@ -6,18 +6,18 @@ abstract contract PermitExt {
     bytes32 public constant PERMIT_TYPEHASH =
         keccak256("Permit(address owner,address spender,uint256 value,uint256 nonce,uint256 deadline)");
 
-    string public name;
+    string public _name;
     mapping(address => uint256) public nonces;
 
-    constructor(string memory _name) {
-        name = _name;
+    constructor(string memory name_) {
+        _name = name_;
     }
 
     function DOMAIN_SEPARATOR() public view returns (bytes32) {
         return keccak256(
             abi.encode(
                 keccak256("EIP712Domain(string name,string version,uint256 chainId,address verifyingContract)"),
-                keccak256(bytes(name)),
+                keccak256(bytes(_name)),
                 keccak256("1"),
                 block.chainid,
                 address(this)
@@ -34,6 +34,7 @@ abstract contract PermitExt {
         bytes32 structHash = keccak256(abi.encode(PERMIT_TYPEHASH, owner, spender, value, nonces[owner]++, deadline));
         bytes32 digest = keccak256(abi.encodePacked("\x19\x01", DOMAIN_SEPARATOR(), structHash));
         require(ecrecover(digest, v, r, s) == owner, "invalid permit");
+        require(owner != address(0), "bad signature");
         _setAllowance(owner, spender, value);
     }
 }
