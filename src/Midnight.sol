211fc0d15e67fff05e575501f7a35453d372e48d
Adrien Husson <adhusson@gmail.com>
2026-05-13 11:24:14 +0200

    Update src/Midnight.sol
    Signed-off-by: Adrien Husson <adhusson@gmail.com>

--- src/Midnight.sol
+++ src/Midnight.sol
@@ -88,7 +88,7 @@ import {EventsLib} from "./libraries/EventsLib.sol";
 /// @dev To work as expected, all offers in the same group should have the same max values and loan token.
 
 /// OFFER CAPS
-/// @dev Offers can only cap units or assets, not both.
+/// @dev If maxAssets>0, assets are capped to maxAssets, otherwise units are capped to maxUnits.
 /// @dev maxAssets caps max buyer assets if offer.buy is true, and caps max seller assets otherwise.
 /// @dev If only maxAssets is zero, this does not cap the assets. If only maxUnits is zero, this does not cap the units.
 ///
