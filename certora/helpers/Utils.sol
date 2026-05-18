75110713dc8282e280c78266840d990cb8d31609
lilCertora <lilian@certora.com>
2026-05-18 10:37:03 +0200

    utils remove dupplicate

--- certora/helpers/Utils.sol
+++ certora/helpers/Utils.sol
@@ -42,10 +42,6 @@ contract Utils {
         return offer;
     }
 
-    function callbackSuccess() external pure returns (bytes32) {
-        return CALLBACK_SUCCESS;
-    }
-
     function maxTradingFee(uint256 index) external pure returns (uint256) {
         return _maxTradingFee(index);
     }
