e6b955a6f70e5f3281af56d37ae36ca4a9570ec9
peyha <peyha.nicole@outlook.fr>
2026-05-21 14:45:37 +0200

    chore: fmt

--- test/EcrecoverRatifierTest.sol
+++ test/EcrecoverRatifierTest.sol
@@ -5,7 +5,11 @@ pragma solidity ^0.8.0;
 import {Offer} from "../src/interfaces/IMidnight.sol";
 import {CALLBACK_SUCCESS} from "../src/libraries/ConstantsLib.sol";
 import {HashLib} from "../src/ratifiers/libraries/HashLib.sol";
-import {IEcrecoverRatifier, Signature, EIP712_DOMAIN_TYPEHASH} from "../src/ratifiers/interfaces/IEcrecoverRatifier.sol";
+import {
+    IEcrecoverRatifier,
+    Signature,
+    EIP712_DOMAIN_TYPEHASH
+} from "../src/ratifiers/interfaces/IEcrecoverRatifier.sol";
 import {BaseTest} from "./BaseTest.sol";
 
 contract EcrecoverRatifierTest is BaseTest {
