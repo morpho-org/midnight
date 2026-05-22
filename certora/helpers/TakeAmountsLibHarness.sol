feb0e82ae8ab9e09a7efc9ae2bb14bd50066b29c
lilCertora <lilian@certora.com>
2026-05-22 22:07:48 +0200

    add helper

---
+++ certora/helpers/TakeAmountsLibHarness.sol
@@ -0,0 +1,25 @@
+// SPDX-License-Identifier: GPL-2.0-or-later
+// Copyright (c) 2025 Morpho Association
+pragma solidity ^0.8.0;
+
+import {Offer} from "../../src/interfaces/IMidnight.sol";
+import {TakeAmountsLib} from "../../src/periphery/TakeAmountsLib.sol";
+
+/// @dev Thin harness exposing TakeAmountsLib's `internal` library functions to CVL.
+contract TakeAmountsLibHarness {
+    function buyerAssetsToUnits(address midnight, bytes32 id, Offer memory offer, uint256 targetBuyerAssets)
+        external
+        view
+        returns (uint256)
+    {
+        return TakeAmountsLib.buyerAssetsToUnits(midnight, id, offer, targetBuyerAssets);
+    }
+
+    function sellerAssetsToUnits(address midnight, bytes32 id, Offer memory offer, uint256 targetSellerAssets)
+        external
+        view
+        returns (uint256)
+    {
+        return TakeAmountsLib.sellerAssetsToUnits(midnight, id, offer, targetSellerAssets);
+    }
+}
