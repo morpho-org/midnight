8b91b6e8f20c9ddf3c647973a2bb167c05591a4a
MathisGD <74971347+MathisGD@users.noreply.github.com>
2026-05-28 17:47:56 +0200

    cleaner no-op prevention

--- test/LiquidationTest.sol
+++ test/LiquidationTest.sol
@@ -156,6 +156,16 @@ contract LiquidationTest is BaseTest {
         midnight.liquidate(market, 0, 1, 1, borrower, false, address(this), address(0), "");
     }
 
+    function testLiquidateNonBorrower(uint256 units) public {
+        units = bound(units, 1, MAX_UNITS);
+        collateralize(market, borrower, units);
+        setupMarket(market, units);
+
+        address nonBorrower = makeAddr("nonBorrower");
+        vm.expectRevert(IMidnight.NotBorrower.selector);
+        midnight.liquidate(market, 0, 0, 0, nonBorrower, false, address(this), address(0), "");
+    }
+
     function testLiquidatePostMaturityPathBeforeMaturity(uint256 units, uint256 liquidationOraclePrice) public {
         units = bound(units, 1, MAX_UNITS);
         liquidationOraclePrice = bound(liquidationOraclePrice, 0, ORACLE_PRICE_SCALE - 1);
