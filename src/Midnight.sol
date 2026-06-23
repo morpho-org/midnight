3818b04cad09d3a3751fa47f92195aebfb6ac96a
MathisGD <74971347+MathisGD@users.noreply.github.com>
2026-06-23 13:48:40 +0200

    Apply suggestion from @MathisGD
    Signed-off-by: MathisGD <74971347+MathisGD@users.noreply.github.com>

--- src/Midnight.sol
+++ src/Midnight.sol
@@ -210,12 +210,6 @@ contract Midnight is IMidnight {
     address public feeSetter;
     address public feeClaimer;
     address public tickSpacingSetter;
-mapping(uint256 lltv => bool) public isLltvAllowed;
-mapping(uint256 liquidationCursor => bool) public isLiquidationCursorEnabled;
-address public roleSetter;
-address public feeSetter;
-address public feeClaimer;
-address public tickSpacingSetter;
 
     /// CONSTRUCTOR ///
 
