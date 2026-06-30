b57fee0195b991c3cd9d0e84aeb8a3a077818753
MathisGD <74971347+MathisGD@users.noreply.github.com>
2026-06-30 17:42:42 +0200

    fmt

--- src/Midnight.sol
+++ src/Midnight.sol
@@ -151,8 +151,8 @@ import {IMidnight, Market, Offer, CollateralParams, MarketState, Position} from 
 /// LIVENESS
 /// @dev If an activated collateral oracle reverts on price, liquidate reverts.
 /// @dev If the supplied collateral oracle reverts on price, the activation of that collat through supplyCollateral
-/// reverts. @dev If an activated collateral oracle reverts on price, isHealthy, withdrawCollateral and take revert when
-/// the user
+/// reverts.
+/// @dev If an activated collateral oracle reverts on price, isHealthy, withdrawCollateral and take revert when the user
 /// (seller for take) has non-zero debt.
 /// @dev If the liquidated collateral oracle returns 0 on price, liquidate with repaid input reverts.
 /// @dev If an activated collateral oracle returns a price such that the user's collateral quoted in loan token is
