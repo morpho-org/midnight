621c9532e7ff553f583c1482e0c2a1918f58a907
MathisGD <74971347+MathisGD@users.noreply.github.com>
2026-07-21 15:31:14 +0200

    move import files

--- test/imports/MorphoImport.sol
+++ test/imports/MorphoImport.sol
@@ -2,5 +2,5 @@
 // Copyright (c) 2026 Morpho Association
 pragma solidity 0.8.19;
 
-// Force Foundry to compile Morpho Blue without importing it in the tests.
+// Force Foundry to compile Morpho Blue for deployCode in tests.
 import {Morpho} from "../../lib/morpho-blue/src/Morpho.sol";
