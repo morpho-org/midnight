07ff6337cd6d6a51f132ed9419fd2aceeac7b22d
MathisGD <74971347+MathisGD@users.noreply.github.com>
2026-07-30 16:26:05 +0200

    Update src/periphery/BlueBuyCallback.sol
    Co-authored-by: Quentin Garchery <garchery.quentin@gmail.com>
    Signed-off-by: MathisGD <74971347+MathisGD@users.noreply.github.com>

--- src/periphery/BlueBuyCallback.sol
+++ src/periphery/BlueBuyCallback.sol
@@ -85,7 +85,7 @@ contract BlueBuyCallback is IBlueBuyCallback {
 
     /// @dev Max buyerAssets amount that the callback can handle.
     /// @dev Takers receive the amount to take per offer from the routing layer. But the routing layer is
-    /// asynchronous/offchain, and might not be up to date on the chain's latest state. To counter this, bundles can
+    /// asynchronous/offchain, and might not be up to date on the chain's latest state. To counter this, takers can
     /// query atomically this function to cap their take.
     /// @dev Ignores some static reasons why the bound might be smaller, such as wrong loan token, wrong owner... But it is easy for the routing layer to take that into account.
     /// @dev Reverts if data is not well formed.
