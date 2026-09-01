7a917f20b1c6d64baee3865ce00f3be2da7a7d8b
MathisGD <74971347+MathisGD@users.noreply.github.com>
2026-09-01 11:29:31 +0200

    Apply batched suggestions from code review
    Co-authored-by: MathisGD <74971347+MathisGD@users.noreply.github.com>
    Signed-off-by: MathisGD <74971347+MathisGD@users.noreply.github.com>

--- src/periphery/blue-fallback-rolling/BlueFallbackRolling.sol
+++ src/periphery/blue-fallback-rolling/BlueFallbackRolling.sol
@@ -22,8 +22,7 @@ import {SafeApproveLib} from "../libraries/SafeApproveLib.sol";
 /// which case the destination position debt and collateral can be difficult to predict.
 /// @dev Contrary to Midnight, Blue positions can be liquidated because of interest accrual, which should be taken into
 /// account when deciding/approving the rolling configuration.
-/// @dev Nothing prevents rollers from leaving a small amount of debt on Midnight that would be unprofitable to roll
-/// though incentiveAtEnd can mitigate the size of the remaining debt.
+/// @dev Nothing prevents rollers from leaving a small amount of debt on Midnight that would be unprofitable to roll.
 /// @dev Inherits the token safety requirements of Midnight and Blue.
 contract BlueFallbackRolling is IBlueFallbackRolling {
     using MarketParamsLib for MarketParams;
